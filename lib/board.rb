ef930a7bd347e54149fd23d3cd42a37a9d61c88d
D.J. Crosby <taladan@gmail.com>
2023-03-11 18:07:07 -0500

    Create lib directory for class and module files
    Created simple board.rb for the game board class, set the main game
    file up to require the the class file in.

---
+++ lib/board.rb
@@ -0,0 +1,7 @@
+class Board
+  attr_reader :player1, :player2
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,1 +1,7 @@
 # tictactoe.rb
+
+require "./lib/board"
+
+my_game = Board.new("Human", "Computer")
+puts my_game.player1
+puts my_game.player2
