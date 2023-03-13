d85927eb8436e9bf5eb927dab1b40141d45731e5
D.J. Crosby <taladan@gmail.com>
2023-03-13 11:15:28 -0400

    Refactor board and state checks
    To begin with, I was storing values in their own variables
    (a0, a1, a2, b0...c2), but I was having issues working with state
    checking for win state.  I refactored to using a matrix of values
    and set up checks for rows, columns, diagonals and blanks.  Currently
    the check_win_state will go through each check and set the @game_over
    value as well as incrementing the winning player's score appropriately.

--- lib/board.rb
+++ lib/board.rb
@@ -1,47 +1,114 @@
+require "matrix"
+
 class Board
-  attr_reader :scores
-
-  @scores = { p1: 0, p2: 0 }
-  @separator_line = "________________|_______________|_________________"
-  @space_line = "                |               |                "
-  @a0 = "\tX\t|"
-  @a1 = "\tO\t|"
-  @a2 = "\tX"
-  @b0 = "\tO\t|"
-  @b1 = "\tX\t|"
-  @b2 = "\tO"
-  @c0 = "\tX\t|"
-  @c1 = "\tO\t|"
-  @c2 = "\tX"
-
+  attr_reader :winner
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @winner = ""
+    @scores = { p1: 0, p2: 0, ties: 0 }
+    @separator_line = "________________|_______________|_________________"
+    @space_line = "                |               |                "
+    @board_matrix = Matrix[%w[X O O], %w[X O X], %w[X X O]]
+    #@board_matrix = Matrix[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    @row0 =
+      "\t#{@board_matrix[0, 0]}\t|\t#{@board_matrix[0, 1]}\t|\t#{@board_matrix[0, 2]}"
+    @row1 =
+      "\t#{@board_matrix[1, 0]}\t|\t#{@board_matrix[1, 1]}\t|\t#{@board_matrix[1, 2]}"
+    @row2 =
+      "\t#{@board_matrix[2, 0]}\t|\t#{@board_matrix[2, 1]}\t|\t#{@board_matrix[2, 2]}"
+    @game_over = false
   end
 
-  def self.clear_screen
-    system("clear") || system("cls")
-  end
-
-  def self.draw_board
+  def draw_board()
     # figure out how to draw the board
-    clear_screen()
-    print self.game_grid()
+    system("clear") || system("cls")
+    print game_grid()
   end
 
-  def self.game_grid
+  def game_over?
+    check_win_state()
+    draw_board()
+    @game_over
+  end
+
+  private
+
+  def check_win_state()
+    if blank?()
+      @game_over = false
+      @winner = ""
+    end
+    check_cols()
+    check_rows()
+    check_diagonals()
+  end
+
+  def check_diagonals()
+    if (0..2).map { |cell| @board_matrix[cell, cell] }.all?("X") ||
+         2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?("X")
+      @winner = "Player 1"
+      @scores[:p1] += 1
+      @game_over = true
+    elsif (0..2).map { |cell| @board_matrix[cell, cell] }.all?("O") ||
+          2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?("O")
+      @winner = "Player 2"
+      @scores[:p2] += 1
+      @game_over = true
+    end
+  end
+
+  def check_rows()
+    rows = (0..2).to_a
+    rows.each do |row|
+      if @board_matrix.row(row).all?("X")
+        @winner = "Player 1"
+        @scores[:p1] += 1
+        @game_over = true
+      elsif @board_matrix.row(row).all?("O")
+        @winner = "Player 2"
+        @scores[:p2] += 1
+        @game_over = true
+      end
+    end
+  end
+
+  def check_cols()
+    columns = (0..2).to_a
+    columns.each do |col|
+      if @board_matrix.column(col).all?("X")
+        @winner = "Player 1"
+        @scores[:p1] += 1
+        @game_over = true
+      elsif @board_matrix.column(col).all?("O")
+        @winner = "Player 2"
+        @scores[:p2] += 1
+        @game_over = true
+      end
+    end
+  end
+
+  def blank?
+    @board_matrix.all? { |elem| elem == " " }
+  end
+
+  def game_grid
+    # do not change any spacing within this declaration
     <<~BOARD
-    #{@space_line}
-     #{@a0}#{@a1}#{@a2}
+    #{@space_line}\tScores:
+    #{@row0}
+    #{@separator_line}\t\tPlayer 1: #{@scores[:p1]}
+    #{@space_line}\t\tPlayer 2: #{@scores[:p2]}
+    #{@row1}\t\t\tTies:     #{@scores[:ties]}
     #{@separator_line}
     #{@space_line}
-     #{@b0}#{@b1}#{@b2}
-    #{@separator_line}
-    #{@space_line}
-     #{@c0}#{@c1}#{@c2}
+    #{@row2}
     #{@space_line}
     BOARD
   end
 
-  draw_board()
+  def all_equal?(arr)
+    # To fix
+    arr.uniq.size == 1
+  end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -2,6 +2,25 @@
 
 require "./lib/board"
 
-my_game = Board.new("Human", "Computer")
-puts my_game.player1
-puts my_game.player2
+system("clear") || system("cls")
+msg = "Hello, would you like to play a game of Tic-Tac-Toe? (Y/N)"
+answer = ""
+
+msg.each_char do |c|
+  putc c
+  sleep 0.10
+end
+
+until %w[y n].include? answer
+  puts "\n"
+  puts "Please choose Y or N."
+  answer = gets.chomp.downcase
+end
+
+if answer == "y"
+  my_game = Board.new("Human", "Computer")
+  my_game.draw_board()
+  my_game.game_over?
+elsif answer == "n"
+  exit(true)
+end
