f372147929da7869a27ef713447f37fb17440d84
D.J. Crosby <taladan@gmail.com>
2023-03-13 18:40:27 -0400

    Begin implementation of player objects

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,21 @@
 require "matrix"
+require "./lib/cells"
+include Cells
 
 class Board
-  attr_reader :winner
-  def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
+  attr_accessor :game_over
+  def initialize(p1, p2)
+    @player1 = p1
+    @player2 = p2
     @winner = ""
+    # This will be deprecated after full implementation of player objects
     @scores = { p1: 0, p2: 0, ties: 0 }
     @separator_line = "   _____________|_______________|______________   "
     @space_line = "                |               |                "
     # Currently using for testing win-state settings
-    # @board_matrix = Matrix[%w[X O O], %w[X O X], %w[X X O]]
+    @board_matrix = Matrix[%w[X O O], %w[X O X], %w[X X O]]
     # Uncomment before finish
-    @board_matrix = Matrix[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    # @board_matrix = Matrix[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
     @row_top = " \t0\t \t1\t \t2"
     @row0 =
       "A\t#{@board_matrix[0, 0]}\t|\t#{@board_matrix[0, 1]}\t|\t#{@board_matrix[0, 2]}"
@@ -35,14 +38,13 @@ class Board
 
   def play_piece(player, choice)
     cell = get_cell(choice)
-    p cell
     if player == "Player 1"
       piece = "X"
     else
       piece = "O"
     end
 
-    if self.playable?(cell)
+    if self.playable?(@board_matrix, cell)
       puts "playing #{piece} in #{choice}"
       @board_matrix[cell[0], cell[1]] = piece
     else
@@ -52,39 +54,14 @@ class Board
 
   private
 
-  # convert player choice to a matrix call (2 element array)
-  def get_cell(choice)
-    case choice
-    when "a0"
-      [0, 0]
-    when "a1"
-      [0, 1]
-    when "a2"
-      [0, 2]
-    when "b0"
-      [1, 0]
-    when "b1"
-      [1, 1]
-    when "b2"
-      [1, 2]
-    when "c0"
-      [2, 0]
-    when "c1"
-      [2, 1]
-    when "c2"
-      [2, 2]
-    end
-  end
-
-  # Cell isn't playable if an X or O is already in it
-  def playable?(cell)
-    @board_matrix[cell[0], cell[1]] == " "
-  end
-
   def check_win_state()
     if blank?()
       @game_over = false
       @winner = ""
+    elsif full?()
+      @game_over = true
+      @winner = "Tie"
+      @scores[:ties] += 1
     end
     check_cols()
     check_rows()
@@ -95,12 +72,12 @@ class Board
     if (0..2).map { |cell| @board_matrix[cell, cell] }.all?("X") ||
          2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?("X")
       @winner = "Player 1"
-      @scores[:p1] += 1
+      @player1.score += 1
       @game_over = true
     elsif (0..2).map { |cell| @board_matrix[cell, cell] }.all?("O") ||
           2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?("O")
       @winner = "Player 2"
-      @scores[:p2] += 1
+      @player2.score += 1
       @game_over = true
     end
   end
@@ -110,11 +87,11 @@ class Board
     rows.each do |row|
       if @board_matrix.row(row).all?("X")
         @winner = "Player 1"
-        @scores[:p1] += 1
+        @player1.score += 1
         @game_over = true
       elsif @board_matrix.row(row).all?("O")
         @winner = "Player 2"
-        @scores[:p2] += 1
+        @player2.score += 1
         @game_over = true
       end
     end
@@ -125,11 +102,11 @@ class Board
     columns.each do |col|
       if @board_matrix.column(col).all?("X")
         @winner = "Player 1"
-        @scores[:p1] += 1
+        @player1.score += 1
         @game_over = true
       elsif @board_matrix.column(col).all?("O")
         @winner = "Player 2"
-        @scores[:p2] += 1
+        @player2.score += 1
         @game_over = true
       end
     end
@@ -137,6 +114,10 @@ class Board
 
   def blank?
     @board_matrix.all? { |elem| elem == " " }
+  end
+
+  def full?
+    @board_matrix.none? { |elem| elem == " " }
   end
 
   # We have to update the game grid each time a play is made
@@ -149,15 +130,20 @@ class Board
       "C\t#{@board_matrix[2, 0]}\t|\t#{@board_matrix[2, 1]}\t|\t#{@board_matrix[2, 2]}"
   end
 
+  def put_status()
+    print "Winner of this round: #{@winner}" if @game_over
+  end
+
   def game_grid
-    update_grid
+    update_grid()
+    put_status()
     # do not change any spacing within this declaration
     <<~BOARD
     #{@row_top}
     #{@space_line}\tScores:
     #{@row0}
-    #{@separator_line}\t\tPlayer 1: #{@scores[:p1]}
-    #{@space_line}\t\tPlayer 2: #{@scores[:p2]}
+    #{@separator_line}\t\t#{@player1}: #{@player1.score}
+    #{@space_line}\t\t#{@player2}: #{@player2.score}
     #{@row1}\t\t\tTies:     #{@scores[:ties]}
     #{@separator_line}
     #{@space_line}
