8a5b12ccc0731da71ecbbf1cc9f64907c27455df
D.J. Crosby <taladan@gmail.com>
2023-03-14 14:50:43 -0400

    Open access to variables to instances

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,16 @@
 # lib/player.rb
 
 class Player
-  attr_reader :name, :piece
+  attr_reader :name, :piece, :type
   attr_accessor :score
   def initialize(name, piece)
     @name = name
     @piece = piece
     @score = 0
+    @type = "human"
   end
+
+  protected
+
+  attr_accessor :name, :piece, :type
 end
