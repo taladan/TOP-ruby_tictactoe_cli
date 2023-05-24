a6b043e8a22e208f2467407c3d74e6a2062f6c27
D.J. Crosby <taladan@gmail.com>
2023-05-24 14:57:25 -0400

    fix: linter format

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,23 @@
-require "./lib/cells"
-require "./lib/messaging"
+require './lib/cells'
+require './lib/messaging'
 include Cells
 include Messaging
 
 class Board
   attr_accessor :game_over, :ties
   attr_reader :board_matrix, :winner
+
   def initialize(p1, p2)
     @player1 = p1
     @player2 = p2
-    @winner = ""
+    @winner = ''
     # This will be deprecated after full implementation of player objects
-    @separator_line = "   _____________|_______________|______________   "
-    @space_line = "                |               |                "
+    @separator_line = '   _____________|_______________|______________   '
+    @space_line = '                |               |                '
     # Currently using for testing win-state settings
     # @board_matrix = Matrix[%w[X O O], %w[X O X], %w[X X O]]
     # Uncomment before finish
-    @board_matrix = Matrix[[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    @board_matrix = Matrix[[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     @row_top = " \t0\t \t1\t \t2"
     @row0 =
       "A\t#{@board_matrix[0, 0]}\t|\t#{@board_matrix[0, 1]}\t|\t#{@board_matrix[0, 2]}"
@@ -27,13 +28,13 @@ class Board
     @game_over = false
   end
 
-  def draw_board()
-    system("clear") || system("cls")
-    print game_grid()
+  def draw_board
+    system('clear') || system('cls')
+    print game_grid
   end
 
-  def game_over
-    check_win_state()
+  def game_over?
+    check_win_state
     @game_over
   end
 
@@ -46,71 +47,71 @@ class Board
     # is the choice playable?
     #
     # if spot is playable? Play spot.  Otherwise prompt for a new spot and try to play again.
-    cell = get_cell(get_player_choice())
+    cell = get_cell(get_player_choice)
     piece = player.piece
     until playable?(@board_matrix, cell)
-      write("That spot is already taken./n")
-      cell = get_cell(get_player_choice())
+      write('That spot is already taken./n')
+      cell = get_cell(get_player_choice)
     end
     @board_matrix[cell[0], cell[1]] = piece
   end
 
   private
 
-  def get_player_choice()
-    choice = ""
+  def get_player_choice
+    choice = ''
     until input_valid?(choice)
-      write("Please choose a spot to place your piece:> ")
+      write('Please choose a spot to place your piece:> ')
       choice = gets.chomp
     end
     choice
   end
 
-  def check_win_state()
-    if blank?()
+  def check_win_state
+    if blank?
       @game_over = false
-      @winner = ""
-    elsif full?()
+      @winner = ''
+    elsif full?
       @game_over = true
-      @winner = "Tie"
+      @winner = 'Tie'
     end
-    check_cols()
-    check_rows()
-    check_diagonals()
+    check_cols
+    check_rows
+    check_diagonals
   end
 
-  def check_diagonals()
-    if (0..2).map { |cell| @board_matrix[cell, cell] }.all?("X") ||
-         2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?("X")
+  def check_diagonals
+    if (0..2).map { |cell| @board_matrix[cell, cell] }.all?('X') ||
+       2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?('X')
       @winner = @player1
       @game_over = true
-    elsif (0..2).map { |cell| @board_matrix[cell, cell] }.all?("O") ||
-          2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?("O")
+    elsif (0..2).map { |cell| @board_matrix[cell, cell] }.all?('O') ||
+          2.downto(0).map { |cell| @board_matrix[cell, cell] }.all?('O')
       @winner = @player2
       @game_over = true
     end
   end
 
-  def check_rows()
+  def check_rows
     rows = (0..2).to_a
     rows.each do |row|
-      if @board_matrix.row(row).all?("X")
+      if @board_matrix.row(row).all?('X')
         @winner = @player1
         @game_over = true
-      elsif @board_matrix.row(row).all?("O")
+      elsif @board_matrix.row(row).all?('O')
         @winner = @player2
         @game_over = true
       end
     end
   end
 
-  def check_cols()
+  def check_cols
     columns = (0..2).to_a
     columns.each do |col|
-      if @board_matrix.column(col).all?("X")
+      if @board_matrix.column(col).all?('X')
         @winner = @player1
         @game_over = true
-      elsif @board_matrix.column(col).all?("O")
+      elsif @board_matrix.column(col).all?('O')
         @winner = @player2
         @game_over = true
       end
@@ -118,11 +119,11 @@ class Board
   end
 
   def blank?
-    @board_matrix.all? { |elem| elem == " " }
+    @board_matrix.all? { |elem| elem == ' ' }
   end
 
   def full?
-    @board_matrix.none? { |elem| elem == " " }
+    @board_matrix.none? { |elem| elem == ' ' }
   end
 
   # We have to update the game grid each time a play is made
@@ -135,25 +136,25 @@ class Board
       "C\t#{@board_matrix[2, 0]}\t|\t#{@board_matrix[2, 1]}\t|\t#{@board_matrix[2, 2]}"
   end
 
-  def put_status()
+  def put_status
     print "Winner of this round: #{@winner}" if @game_over
   end
 
   def game_grid
-    update_grid()
-    put_status()
+    update_grid
+    put_status
     # do not change any spacing within this declaration
     <<~BOARD
-    #{@row_top}
-    #{@space_line}\tScores:
-    #{@row0}
-    #{@separator_line}\t\t#{@player1.name}: #{@player1.score}
-    #{@space_line}\t\t#{@player2.name}: #{@player2.score}
-    #{@row1}
-    #{@separator_line}
-    #{@space_line}
-    #{@row2}
-    #{@space_line}
+      #{@row_top}
+      #{@space_line}\tScores:
+      #{@row0}
+      #{@separator_line}\t\t#{@player1.name}: #{@player1.score}
+      #{@space_line}\t\t#{@player2.name}: #{@player2.score}
+      #{@row1}
+      #{@separator_line}
+      #{@space_line}
+      #{@row2}
+      #{@space_line}
     BOARD
   end
 end
