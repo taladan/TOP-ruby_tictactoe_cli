113fa57918d058375e2bac0a8baa5a92ef8d7a68
D.J. Crosby <taladan@gmail.com>
2023-05-28 07:51:53 -0400

    linter formatting

--- lib/cells.rb
+++ lib/cells.rb
@@ -3,30 +3,30 @@
 module Cells
   def get_cell(choice)
     case choice
-    when "a0"
+    when 'a0'
       [0, 0]
-    when "a1"
+    when 'a1'
       [0, 1]
-    when "a2"
+    when 'a2'
       [0, 2]
-    when "b0"
+    when 'b0'
       [1, 0]
-    when "b1"
+    when 'b1'
       [1, 1]
-    when "b2"
+    when 'b2'
       [1, 2]
-    when "c0"
+    when 'c0'
       [2, 0]
-    when "c1"
+    when 'c1'
       [2, 1]
-    when "c2"
+    when 'c2'
       [2, 2]
     end
   end
 end
 
 def playable?(matrix, cell)
-  matrix[cell[0], cell[1]] == " "
+  matrix[cell[0], cell[1]] == ' '
 end
 
 def input_valid?(choice)
