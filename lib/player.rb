51091a0b86f84b09cf74879771ee2bb8d12aa9f3
D.J. Crosby <taladan@gmail.com>
2023-05-28 07:45:19 -0400

    fix: remove unused variable

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,12 @@
 # lib/player.rb
 
 class Player
-  attr_reader :name, :piece, :type
+  attr_reader :name, :piece
   attr_accessor :score, :name
+
   def initialize(name, piece)
     @name = name
     @piece = piece
     @score = 0
-    @type = "human"
   end
 end
